30de1a57412bfa510b5252369a384ef2a82cb07b
pravee42 <darkparadise877@gmail.com>
2024-11-22 22:06:51 +0530

    upf

--- controllers/examController.js
+++ controllers/examController.js
@@ -1,26 +1,25 @@
 const Exam = require('../models/Exam');
 const activeChannel = require('../models/activeChannel');
 const Settings = require('../models/Settings');
-const { emitMessage } = require('../utils/socketSetup');
+const {emitMessage} = require('../utils/socketSetup');
 
 const createMCQSettings = async (req, res) => {
   try {
     const settings = await Settings.findOneAndUpdate(
       {},
-      { level1PassPercent: req.body.level1PassPercent },
-      { upsert: true, new: true }
+      {level1PassPercent: req.body.level1PassPercent},
+      {upsert: true, new: true},
     );
     res.status(200).json(settings);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({message: error.message});
   }
 };
 
 const createExamResult = async (req, res) => {
-  const { userId, level, round, timeTaken, mistakes, status } = req.body;
+  const {userId, level, round, timeTaken, mistakes, status} = req.body;
 
-  console.log(req.body)
-
+  console.log(req.body);
 
   try {
     if (level >= 2) {
@@ -45,7 +44,7 @@ const createExamResult = async (req, res) => {
       }
     }
 
-    const userAttempts = await Exam.find({ userId, level, round }).sort({
+    const userAttempts = await Exam.find({userId, level, round}).sort({
       createdAt: 1,
     });
 
@@ -54,7 +53,8 @@ const createExamResult = async (req, res) => {
       const oldestAttempt = userAttempts[userAttempts.length - 1];
       oldestAttempt.timeTaken = timeTaken;
       oldestAttempt.mistakes = mistakes;
-      oldestAttempt.status = status ? "pass" : "fail";
+      oldestAttempt.status = status ? 'pass' : 'fail';
+      oldestAttempt.scorePercentage = status ? 100 : 0;
       oldestAttempt.attempts += 1;
 
       examResult = await oldestAttempt.save();
@@ -66,40 +66,42 @@ const createExamResult = async (req, res) => {
         attempts: 1,
         timeTaken,
         mistakes,
-        status: status ? "pass" : "fail",
+        status: status ? 'pass' : 'fail',
+        scorePercentage: status ? 100 : 0,
       }).save();
     }
 
     try {
-      const deviceToUpdate = await activeChannel.find({
-        userId,
-        status: 'started'
-      })
-      .sort({ lastConnectedTime: -1 })
-      .limit(1);
+      const deviceToUpdate = await activeChannel
+        .find({
+          userId,
+          status: 'started',
+        })
+        .sort({lastConnectedTime: -1})
+        .limit(1);
 
       if (deviceToUpdate?.[0]) {
         emitMessage(deviceToUpdate[0].device, examResult);
       } else {
-        console.log("no device to update has been added");
+        console.log('no device to update has been added');
       }
     } catch (error) {
-      console.log(error.message, "error emitting");
+      console.log(error.message, 'error emitting');
     }
 
     await activeChannel.findOneAndUpdate(
-      { device: deviceToUpdate?.[0]?.device },
+      {device: deviceToUpdate?.[0]?.device},
       {
         userId: userId,
         status: 'ended',
       },
-      { new: true, upsert: true }
+      {new: true, upsert: true},
     );
 
     res.status(userAttempts.length >= 5 ? 200 : 201).json(examResult);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({message: error.message});
   }
 };
 
-module.exports = { createMCQSettings, createExamResult };
+module.exports = {createMCQSettings, createExamResult};
